a5c3d5e43e4c91d24c98718363355165f993ce6c
hnhoangdz <snacky0907@gmail.com>
2022-01-01 01:28:33 +0700

    upload

--- assets/images/bai9/x.py
+++ assets/images/bai9/x.py
@@ -1,37 +1,15 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
-def convolute(X, kernel, stride = 1, padding = 0):
-    """
-    Parameters
-    ----------
-    X : Grayscale Image - Numpy Array
-    kernel : Numpy Array
-    stride : int
-    padding : int
-    Returns
-    -------
-    Y : Convoluted Image - Numpy Array
 
-    """
-    H,W = X.shape
-    k_h,k_w = kernel.shape
-    height = (H+2*padding-k_h)//stride+1 
-    width = (W+2*padding-k_w)//stride+1
-    Y = np.zeros((height,width))
-    for i in range(0,H-k_h,stride):
-      for j in range(0,W-k_w,stride):
-        if (i<height and j<width):
-          Y[i,j] = np.sum(np.multiply(X[i:i+k_h,j:j+k_w],kernel))
-    return Y
+image = cv2.imread('test.png')
 
-img = cv2.imread('ronaldo.jpg')
-img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-kernel = np.ones((5,5),np.float32)/25
-imgSmooth = cv2.filter2D(img,-1,kernel)
-
-plt.subplot(121),plt.imshow(img),plt.title('Original')
-plt.xticks([]), plt.yticks([])
-plt.subplot(122),plt.imshow(imgSmooth),plt.title('Averaging')
-plt.xticks([]), plt.yticks([])
+small_k = cv2.medianBlur(image, 3)
+medium_k = cv2.medianBlur(image, 5)
+big_k = cv2.medianBlur(image, 7)
+plt.figure(figsize=(16, 5))
+plt.subplot(151),plt.imshow(image),plt.title('Origin Image'),plt.axis(False)
+plt.subplot(152),plt.imshow(small_k),plt.title('k = 3'),plt.axis(False)
+plt.subplot(153),plt.imshow(medium_k),plt.title('k = 5'),plt.axis(False)
+plt.subplot(154),plt.imshow(big_k),plt.title('k = 7'),plt.axis(False)
 plt.show()
