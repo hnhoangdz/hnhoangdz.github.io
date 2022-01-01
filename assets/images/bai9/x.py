b54e5fb73f58e15ece1822d4139ff992ee3a1387
hnhoangdz <snacky0907@gmail.com>
2022-01-01 14:53:18 +0700

    upload b9

--- assets/images/bai9/x.py
+++ assets/images/bai9/x.py
@@ -2,14 +2,33 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-image = cv2.imread('test.png')
+def scale_to_0_255(img):
+    min_val = np.min(img)
+    max_val = np.max(img)
+    new_img = (img - min_val) / (max_val - min_val) # 0-1
+    new_img *= 255
+    return new_img
 
-small_k = cv2.medianBlur(image, 3)
-medium_k = cv2.medianBlur(image, 5)
-big_k = cv2.medianBlur(image, 7)
-plt.figure(figsize=(16, 5))
-plt.subplot(151),plt.imshow(image),plt.title('Origin Image'),plt.axis(False)
-plt.subplot(152),plt.imshow(small_k),plt.title('k = 3'),plt.axis(False)
-plt.subplot(153),plt.imshow(medium_k),plt.title('k = 5'),plt.axis(False)
-plt.subplot(154),plt.imshow(big_k),plt.title('k = 7'),plt.axis(False)
-plt.show()
+def my_canny(img, min_val, max_val, sobel_size=3, is_L2_gradient=False):
+    """
+    Try to implement Canny algorithm in OpenCV tutorial @ https://docs.opencv.org/master/da/d22/tutorial_py_canny.html
+    """
+    
+    #2. Noise Reduction
+    smooth_img = cv2.GaussianBlur(img, ksize=(5, 5), sigmaX=1, sigmaY=1)
+    
+    #3. Finding Intensity Gradient of the Image
+    Gx = cv2.Sobel(smooth_img, cv2.CV_64F, 1, 0, ksize=sobel_size)
+    Gy = cv2.Sobel(smooth_img, cv2.CV_64F, 0, 1, ksize=sobel_size)
+        
+    if is_L2_gradient:
+        edge_gradient = np.sqrt(Gx*Gx + Gy*Gy)
+    else:
+        edge_gradient = np.abs(Gx) + np.abs(Gy)
+    
+    angle = np.arctan2(Gy, Gx) * 180 / np.pi
+    return angle
+
+img = cv2.imread('ronaldo.jpg',0)
+my_canny = my_canny(img, min_val=100, max_val=200)
+print(my_canny)
