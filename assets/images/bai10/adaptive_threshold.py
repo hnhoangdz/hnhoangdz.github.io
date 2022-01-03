b941d654fb4bb09380293586d166508c6370aa6b
hnhoangdz <snacky0907@gmail.com>
2022-01-04 00:18:57 +0700

    b10

--- assets/images/bai10/adaptive_threshold.py
+++ assets/images/bai10/adaptive_threshold.py
@@ -2,65 +2,40 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 
+gray_img = cv2.imread('text.jpg',0)
+blurred = cv2.GaussianBlur(gray_img,(5,5),0)
 
-def otsu_implement(gray):
-    # Mean weight to calculate probability
-    mean_weight = 1.0/gray.shape[0] * gray.shape[1]
-    
-    # Histogram and bins edge horizontal axis
-    his, bins = np.histogram(gray, np.arange(0,256))
-
-    T = -1 # threshold value
-    max_value = -1
-    intensity_arr = np.arange(255)
-    
-    for t in bins[1:-1]:
-        
-        # Weight/probability of Foreground and Background
-        pcb = np.sum(his[:t])
-        pcf = np.sum(his[t:])
-        Wb = pcb * mean_weight
-        Wf = pcf * mean_weight
-        
-        # Mean of Foreground and Background
-        mub = np.sum(intensity_arr[:t]*his[:t]) / float(pcb)
-        muf = np.sum(intensity_arr[t:]*his[t:]) / float(pcf)
-        
-        # Formula must be maximum
-        value = Wb * Wf * (mub - muf) ** 2
-
-        if value > max_value:
-            T = t
-            max_value = value
-    gray[gray > T] = 255
-    gray[gray <= T] = 0
-    return T,gray
-
-def otsu_built(gray):
-    (T, threshImg) = cv2.threshold(gray, 0, 255, \
+# Apply threhold
+_, thresh_img = cv2.threshold(blurred, 125, 255, cv2.THRESH_BINARY)
+_, otsu_img = cv2.threshold(blurred, 0, 255, \
                 cv2.THRESH_BINARY | cv2.THRESH_OTSU)
-    return T,threshImg
+thresh = cv2.adaptiveThreshold(blurred, 255,
+	cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 21, 4)
 
-gray_img = cv2.imread('coin.jpg',0)
-gray_img2 = gray_img.copy() 
+fig = plt.figure(figsize=(12, 10))
 
-otsu_implement_threshold,thresh_img = otsu_implement(gray_img)
-otsu_built_threshold,threshImg = otsu_built(gray_img2)
+fig.add_subplot(221)
+plt.title('Original')
+plt.set_cmap('gray')
+plt.axis('off')
+plt.imshow(gray_img)
 
-plt.figure(figsize=(16, 4))
-plt.subplot(151)
-plt.imshow(gray_img2,cmap='gray')
-plt.title('Origin')
-plt.axis(False)
+fig.add_subplot(222)
+plt.title('T = 125')
+plt.axis('off')
+plt.imshow(thresh_img)
+plt.set_cmap('gray')
 
+fig.add_subplot(223)
+plt.title('Otsu')
+plt.axis('off')
+plt.set_cmap('gray')
+plt.imshow(otsu_img)
 
-plt.subplot(152)
-plt.imshow(thresh_img,cmap='gray')
-plt.title('T-implement = ' + str(otsu_implement_threshold))
-plt.axis(False)
+fig.add_subplot(224)
+plt.title('Adp')
+plt.imshow(thresh)
+plt.axis('off')
+plt.set_cmap('gray')
 
-
-plt.subplot(153)
-plt.imshow(threshImg,cmap='gray')
-plt.title('T-built = ' + str(otsu_built_threshold))
-plt.axis(False)
+plt.show()
